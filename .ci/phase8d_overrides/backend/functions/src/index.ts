b889740a29d95f6e2f8fc6702e06c2f0070f7803
jkcreations0009-bit <jkcreations0009@gmail.com>
2026-08-14 19:36:43 +0530

    Export Phase 8D sharing dashboard callable

---
+++ .ci/phase8d_overrides/backend/functions/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./common/privacy";
+export * from "./common/liveLocationValidation";
+export * from "./common/authPrivacyConsent";
+export * from "./common/liveLocationProtocol";
+export * from "./callables/privacyConsent";
+export * from "./callables/liveLocation";
+export * from "./callables/sharingDashboard";
