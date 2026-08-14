fbd23c5576292ac59c63410325b95db8b77872e7
jkcreations0009-bit <jkcreations0009@gmail.com>
2026-08-14 19:10:33 +0530

    Export Phase 8C live location protocol

---
+++ .ci/phase8c_overrides/backend/functions/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./common/privacy";
+export * from "./common/liveLocationValidation";
+export * from "./common/authPrivacyConsent";
+export * from "./common/liveLocationProtocol";
+export * from "./callables/privacyConsent";
+export * from "./callables/liveLocation";
